1c8fdf14df4f0d36fc7e41a2f3505c26560dfd68
Kenneth Gulbrandsoy <kenneth@discoos.org>
2021-09-04 12:16:53 +0200

    Renamed folders

--- lib/eventuate.dart
+++ lib/eventuate.dart
@@ -2,22 +2,22 @@
 ///
 library eventuate;
 
-export 'src/aggregate/aggregate.dart';
-export 'src/aggregate/aggregate_id.dart';
-export 'src/aggregate/aggregate_state.dart';
-export 'src/aggregate/aggregate_state_store.dart';
-export 'src/aggregate/aggregate_state_type_map.dart';
-export 'src/aggregate/aggregate_store.dart';
-export 'src/aggregate/aggregate_type_map.dart';
-export 'src/events/event.dart';
-export 'src/events/event_handler.dart';
-export 'src/events/event_store.dart';
-export 'src/events/event_store_result.dart';
-export 'src/events/event_type_map.dart';
-export 'src/events/serializer.dart';
-export 'src/events/stream_event.dart';
-export 'src/events/stream_name.dart';
-export 'src/events/version.dart';
+export 'src/state/aggregate.dart';
+export 'src/state/aggregate_id.dart';
+export 'src/state/aggregate_state.dart';
+export 'src/state/aggregate_state_store.dart';
+export 'src/state/aggregate_state_type_map.dart';
+export 'src/state/aggregate_store.dart';
+export 'src/state/aggregate_type_map.dart';
+export 'src/stream/event.dart';
+export 'src/stream/event_handler.dart';
+export 'src/stream/event_store.dart';
+export 'src/stream/event_store_result.dart';
+export 'src/stream/event_type_map.dart';
+export 'src/stream/serializer.dart';
+export 'src/stream/stream_event.dart';
+export 'src/stream/stream_name.dart';
+export 'src/stream/version.dart';
 export 'src/exceptions.dart';
 export 'src/extensions.dart';
 
